baa3ddb53c540f4f341ed16ac48c3c6a11e9819c
AdityaBahl <adityabahl12345@gmail.com>
2023-01-22 03:57:35 +0530

    BFS

---
+++ graph/BFS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+// BFS for an undirected graph, 0 based
+vector<int> bfsOfGraph(int v, vector<int> adj[])
+{
+    int vis[v] = {0};
+    vis[0] = 1;
+    queue<int> q;
+    q.push(0);
+    vector<int> bfs;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}
+int main()
+{
+
+    return 0;
+}
